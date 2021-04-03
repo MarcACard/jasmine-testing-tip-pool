3af8e4d649ddb1e79a251b25763f89d69a35ea9f
Marc Card <marcacard@gmail.com>
2021-04-02 20:27:32 -0700

    add unit tests for servers.js

--- servers.test.js
+++ servers.test.js
@@ -1,6 +1,5 @@
 describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
-    // initialization logic
     serverNameInput.value = 'Alice';
   });
 
@@ -11,7 +10,28 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not work if server name is empty "", when submitServerInfo() is called', function() {
+    serverNameInput.value = ""; 
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  })
+
+  it('should add a new TR to the serverTable on UpdateServerTable()', function () {
+    submitServerInfo(); // updateServerTable() is called within submitServerInfo
+
+    const serverRowNodes = serverTbody.querySelectorAll('tr'); 
+    expect(serverRowNodes.length).toEqual(1);
+
+    const serverRowCols = serverRowNodes[0].querySelectorAll('td');
+    expect(serverRowCols.length).toEqual(2);
+    expect(serverRowCols[0].innerText).toEqual('Alice');
+    expect(serverRowCols[1].innerText).toEqual('$0.00');
+  })
+
   afterEach(function() {
-    // teardown logic
+    serverNameInput.value = '';
+    allServers = {};
+    serverTbody.innerHTML = '';
+    serverId = 0;
   });
 });
